b45dc43821139583840c97c412a7d5a5571a705f
Aaron Levine <allevin@sandia.gov>
2013-09-09 15:41:16 +0000

    Adding new tools directory along with sstbuilder to the trunk

---
+++ tools/sstbuilder/build_linux_debug.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+## Copyright 2009-2013 Sandia Corporation. Under the terms
+## of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+## Government retains certain rights in this software.
+##
+## Copyright (c) 2009-2013, Sandia Corporation
+## All rights reserved.
+##
+## This file is part of the SST software package. For license
+## information, see the LICENSE file in the top level directory of the
+## distribution.
+
+TARGET=LINUX_DEBUG
+
+## Create the Target Directory and cd into it
+mkdir -p $TARGET
+cd $TARGET
+
+## Build the application
+qmake ../sstbuilder.pro -r -spec linux-g++ CONFIG+=debug CONFIG-=release CONFIG+=declarative_debug CONFIG+=qml_debug
+make 
+
+## Move back up one directory
+cd ..
+
---
+++ tools/sstbuilder/build_linux_release.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+## Copyright 2009-2013 Sandia Corporation. Under the terms
+## of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+## Government retains certain rights in this software.
+##
+## Copyright (c) 2009-2013, Sandia Corporation
+## All rights reserved.
+##
+## This file is part of the SST software package. For license
+## information, see the LICENSE file in the top level directory of the
+## distribution.
+
+TARGET=LINUX_RELEASE
+
+## Create the Target Directory and cd into it
+mkdir -p $TARGET
+cd $TARGET
+
+## Build the application
+qmake ../sstbuilder.pro -r -spec linux-g++ CONFIG-=debug CONFIG+=release 
+make 
+
+## Move back up one directory
+cd ..
